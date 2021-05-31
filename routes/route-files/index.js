a268e0d3e67822f3259477bb257f4350a25763f2
Adrian Hernandez adrianrh651@gmail.com <adrianrh651@gmail.com>
2021-05-31 00:43:09 -0700

    change ignore

--- routes/route-files/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-
-var router = express.Router();
-
-router.get('/', function(request, response, next) {
-  index.default(response);
-});
-
-module.exports = router;
--- routes/route-files/invoice.js
+++
@@ -1,8 +0,0 @@
-const invoiceController = require('../../controllers/invoice');
-var express = require('express');
-var router = express();
-
-router.route('/getInvoices')
-    .post(invoiceController.getInvoices);
-
-module.exports = router;
